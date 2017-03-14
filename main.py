e80c283fa95cc91f89d5765935f6bed2e10366c0
Ryan Freckleton <ryan.freckleton@gmail.com>
2017-03-14 11:21:30 -0600

    Draw label

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pyglet
 
 window = pyglet.window.Window(800, 600)
+label = pyglet.text.Label("Hello, World!",
+            x=window.width//2,
+            y=window.height//2)
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
 
 if __name__== '__main__':
     pyglet.app.run()

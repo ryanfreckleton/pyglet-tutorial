b39dc3641f117bc38d0799c03ed28de0e2612d56
Ryan Freckleton <ryan.freckleton@gmail.com>
2017-03-14 14:37:34 -0600

    User input

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+from collections import namedtuple
+
 import pyglet
 
+from pyglet.window import key
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 window = pyglet.window.Window(800, 600)
-label = pyglet.text.Label("Hello, World!",
-            x=window.width//2,
-            y=window.height//2)
+player = Player(window.width//2, window.height//2)
+label = pyglet.text.Label("Player", x=player.x, y=player.y)
 
 @window.event
 def on_draw():
     window.clear()
     label.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    global label
+    if   symbol == key.W: label.y += 10
+    elif symbol == key.A: label.x += -10
+    elif symbol == key.S: label.y += -10
+    elif symbol == key.D: label.x += 10
+
 
 if __name__== '__main__':
     pyglet.app.run()
